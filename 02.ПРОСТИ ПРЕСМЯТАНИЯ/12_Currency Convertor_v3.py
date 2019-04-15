f096c5340954c0dff55bd558c2e4834a1371caf4
mina0805 <mina.burke@hotmail.com>
2019-04-15 13:00:14 -0400

    initial commit

--- 02.ПРОСТИ ПРЕСМЯТАНИЯ/12_Currency Convertor_v3.py
+++ 02.ПРОСТИ ПРЕСМЯТАНИЯ/12_Currency Convertor_v3.py
@@ -7,34 +7,52 @@
 # 1.95583 = 1 EUR
 # 2.53405 = 1 GBP
 ################
+BGN = 1
+USD = 1.79549 * BGN
+EUR = 1.95583 * BGN
+GBP = 2.53405 * BGN
+
+
+amount_in = float(input())
 currency_from = input()
-amount = float(input())
 currency_to = input()
-amount_out = " "
 
-if currency_from == "USD":
-    amount_out = amount/ 1.79549
-elif currency_from == "EUR":
-    amount_out = amount/1.95583
-elif currency_from == "GBP":
-    amount_out = amount/ 2.53405
-elif currency_from == "BGN":
-    amount_out = amount * 1
-elif currency_to == "USD":
-    amount_out = amount * 1.79549
+if currency_from == "USD" and currency_to == "BGN":
+    amount_out = USD * amount_in
+    print(str(round(amount_out, 2)) + " BGN")
+elif currency_from == "EUR" and currency_to == "BGN":
+    amount_out = EUR * amount_in
+    print(str(round(amount_out, 2)) + ' BGN')
+elif currency_from == "GBP" and currency_to == "BGN":
+    amount_out = GBP * amount_in
+    print(str(round(amount_out, 2)) + ' BGN')
 
-if currency_to == "EUR":
-    amount_out = amount * 1.95583
-if currency_to == "GBP":
-    amount_out = amount * 2.53405
-if currency_to == "BGN":
-    amount_out = amount * 1
+elif currency_from == "BGN" and currency_to == "USD":
+    amount_out = amount_in/USD
+    print(str(round(amount_out, 2)) + " USD")
+elif currency_from == "EUR" and currency_to == "USD":
+    amount_out = EUR/USD * amount_in
+    print(str(round(amount_out, 2)) + " USD")
+elif currency_from == "GBP" and currency_to == "USD":
+    amount_out = GBP/USD * amount_in
+    print(str(round(amount_out, 2)) + " USD")
 
-print(round(amount_out, 2))
+elif currency_from == "BGN" and currency_to == "EUR":
+    amount_out = BGN/EUR * amount_in
+    print(str(round(amount_out, 2)) + " EUR")
+elif currency_from == "USD" and currency_to == "EUR":
+    amount_out = USD/EUR * amount_in
+    print(str(round(amount_out, 2)) + " EUR")
+elif currency_from == "GBP" and currency_to == "EUR":
+    amount_out = GBP/EUR * amount_in
+    print(str(round(amount_out, 2)) + " EUR")
 
-
-
-
+elif currency_from == "BGN" and currency_to == "GBP":
+    amount_out = BGN/GBP * amount_in
+    print(str(round(amount_out, 2)) + " GBP")
+elif currency_from == "EUR" and currency_to == "GBP":
+    amount_out = EUR/GBP * amount_in
+    print(str(round(amount_out, 2)) + " GBP")
 
 
 
