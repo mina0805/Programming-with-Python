297a2125e86faf285ea8fc505934f8bbf01af69f
mina0805 <mina.burke@hotmail.com>
2019-04-22 23:29:17 -0400

    update

--- 04.ПО-СЛОЖНИ ЛОГИЧЕСКИ ПРОВЕРКИ/14.On_time_for_exam.py
+++ 04.ПО-СЛОЖНИ ЛОГИЧЕСКИ ПРОВЕРКИ/14.On_time_for_exam.py
@@ -11,35 +11,34 @@ exam_m = int(input("exam minute 0 - 59: "))
 arrival_h = int(input("arrival hour 0 -23: "))
 arrival_m = int(input("arrival minute 0 - 59: "))
 
-on_time_exact = exam_h == arrival_h and exam_m == arrival_m
-# on_time_early =
-   #1 if exam_h //exam_m == 0 and exam
+import math
 
 # late if:
-late_1 = exam_h <= arrival_h or exam_m < arrival_m
-late_2 = exam_h < arrival_h and exam_m == arrival_m
-late_3 = exam_h == arrival_h and exam_m < arrival_m
+late_1 = (exam_h == arrival_h) and (exam_m < arrival_m)
+late_2 = (exam_h < arrival_h) and (exam_m == arrival_m)
+late_3 = (exam_h < arrival_h) and (exam_m < arrival_m)
+
+#on time:
+on_time_1 = (exam_h == arrival_h) and (exam_m == arrival_m)
+on_time_2 = (exam_h == arrival_h) and ((exam_m - arrival_m) <= 30)
+on_time_3 = ((exam_h - arrival_h) == 1) and (arrival_m > exam_m) and ((arrival_m - exam_m) >= 30)
+on_time_4 = ((exam_h - arrival_h) == 1) and (exam_m > arrival_m) and ((exam_m - arrival_m) >= 30)
 
 if late_1 or late_2 or late_3:
     print("late")
-    if late_1 and (arrival_m - exam_m) >= 10:
+    if late_1:
         print((str(arrival_m - exam_m)) + ' minutes after the the start')
-
-    elif late_1 and (arrival_m - exam_m) < 10:
-        print((str(arrival_m - exam_m)), 'minutes after the the start')
     if late_2:
-        print(str(arrival_h - exam_h) + (" hours after the start"))
+        print(str(arrival_h - exam_h) + (":00 hours after the start"))
+    if late_3 and ((arrival_m - exam_m) >= 10):
+        print(str(arrival_h - exam_h) + ":" +(str(arrival_m - exam_m) + (" hours after the start")))
+    if late_3 and ((arrival_m - exam_m)< 10):
+        print(str(arrival_h - exam_h) + ":0" +(str(arrival_m - exam_m) + (" hours after the start")))
 
+elif on_time_1 or on_time_2 or on_time_3 or on_time_4:
+    print('on time')
 
-
-
-
-# on time if:
-#1. exam_h == arrival h and exam_m 30 or less bigger than arrival_m
-#2. exam_h bigger than arrival_h with 0.5 hour or less and exam_m == arrival_m
-#3. exam_h == arrival_h and exam_m bigger than arrival_m with 30 min or less
-
-# early -> everything else
+# early - everything else
 
 
 
