b0cb2a2f35443ae355005644e298b5e3808c40ea
mina0805 <mina.burke@hotmail.com>
2019-05-10 21:55:03 -0400

    update

--- 05.Повторения (цикли)/15.Smart_Lilly.py
+++ 05.Повторения (цикли)/15.Smart_Lilly.py
@@ -8,17 +8,14 @@ for i in range(1, age +1):
     if i % 2 == 1:
         toy_number += 1
     elif i % 2 == 0:
-        money += 9
+        money = ((money + (i*5))-1)
 
-tot_money = money + toy_number * toy_price
-print(money)
-print(tot_money)
-print(toy_number)
+tot_money = money + (toy_price * toy_number)
 
 if tot_money >= laundry_m_price:
-    print('Yes ', str(tot_money - laundry_m_price))
+    print('Yes!', str("%.2f" % (tot_money - laundry_m_price)))
 else:
-    print('No, ', str(laundry_m_price - tot_money))
+    print('No!', str("%.2f" % (laundry_m_price - tot_money)))
 
 
 
