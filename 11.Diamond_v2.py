39e6910d716f5c863bab1e79657964caa64d11fd
mina0805 <mina.burke@hotmail.com>
2019-05-17 22:10:51 -0400

    update

--- 11.Diamond_v2.py
+++ 11.Diamond_v2.py
@@ -1,13 +1,17 @@
 n = int(input())
 left_dash = 1
 right_dash = 1
-dash = 1
 row = 1
+row2 =1
 
 if n % 2 == 1:
     left_dash = (n // row - 1) // 2
     right_dash = (n // row - 1) // 2
     inside_dash = n - 2 - left_dash - right_dash
+    left_dash2 = row2
+    right_dash2 = row2
+    inside_dash2 = n - 2 - left_dash2 - right_dash2
+
     for row in range(1, n // 2 + 2):
         if row == 1:
             print(("-" * left_dash) + "*" + (right_dash * "-"))
@@ -16,5 +20,16 @@ if n % 2 == 1:
         left_dash -= 1
         right_dash -= 1
         inside_dash += 2
+
+    for row2 in range(1, n//2+1):
+        left_dash2 = row2
+        right_dash2 = row2
+        inside_dash2 = n - 2 - left_dash2 - right_dash2
+        if row2 < n//2:
+            print(left_dash2 * "-" + "*" +inside_dash2 * "-"+ "*" + right_dash2 * "-")
+        elif row2 == n//2:
+            print(left_dash2 * "-" + "*" + right_dash2 * "-")
+
+
 
 
