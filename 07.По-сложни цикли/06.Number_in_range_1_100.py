44df4ed3ddeb7f59da22429b375a959fe3f0e8e2
mina0805 <mina.burke@hotmail.com>
2019-05-22 22:53:00 -0400

    update

---
+++ 07.По-сложни цикли/06.Number_in_range_1_100.py
@@ -0,0 +1,6 @@
+n = int(input("Еnter a number in the range [1...100]: "))
+while not 1 <= n <= 100:
+    print('Invalid number!')
+    n = int(input("Еnter a number in the range [1...100]: "))
+if 1 <= n <= 100:
+    print("The number is: " + str(n))
