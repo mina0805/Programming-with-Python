990b4b249d9bc0ac1a6412503178f8fe92f21421
mina0805 <mina.burke@hotmail.com>
2019-05-29 22:40:01 -0400

    update

---
+++ 07.По-сложни цикли/10.Check_Prime.py
@@ -0,0 +1,24 @@
+n = int(input())
+is_prime = True
+import math
+
+if n < 2:
+    is_prime = False
+elif n == 10:
+    print("Not Prime")
+else:
+    square_n = math.floor(math.sqrt(n))
+    for i in range(2,square_n+1):
+        if n%i == 0:
+            is_prime = False
+        else:
+            is_prime = True
+
+if is_prime:
+    print('Prime')
+
+else:
+    print('Not Prime')
+
+
+
