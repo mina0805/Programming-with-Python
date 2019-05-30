3fb921fa03df9005d4af49a6de3249f9c9410979
mina0805 <mina.burke@hotmail.com>
2019-05-29 23:10:17 -0400

    update

--- 07.По-сложни цикли/10.Check_Prime.py
+++ 07.По-сложни цикли/10.Check_Prime.py
@@ -8,8 +8,12 @@ if n < 2:
 else:
     square_n = math.floor(math.sqrt(n))
     for i in range(2,square_n+1):
-        if n/i == n//i:
+        if n%i == 0:
             is_prime = False
+# if n/i == n//i:
+#   is_prime = False
+# input = 10, 70, 150, 200 -> output - prime???
+
         else:
             is_prime = True
 
