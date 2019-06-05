c9e2263372d59be713c6d537372eabc31ca8202d
mina0805 <mina.burke@hotmail.com>
2019-06-04 23:35:39 -0400

    update

---
+++ 07.По-сложни цикли/13.Number_pyramid.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+count = 0
+
+for row in range(n):
+    for row2 in range(row+1):
+        count +=1
+        print(count, "", end = "")
+        if count == n:
+            break
+    if count == n:
+        break
+    print("")
+
+
