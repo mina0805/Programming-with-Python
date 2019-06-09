0e5d867791eb1649108f5cb58492e83c72a0daf5
mina0805 <mina.burke@hotmail.com>
2019-06-09 10:33:55 -0400

    update

--- 07.По-сложни цикли/13.Number_pyramid.py
+++ 07.По-сложни цикли/13.Number_pyramid.py
@@ -3,7 +3,7 @@ n = int(input())
 count = 0
 
 for row in range(n):
-    for row2 in range(row+1):
+    for col in range(row + 1):
         count += 1
         print(count, "", end="")
         if count == n:
