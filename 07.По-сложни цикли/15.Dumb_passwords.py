1fabafe134ec454b6d93631477d9f2d07b2a9225
mina0805 <mina.burke@hotmail.com>
2019-06-22 22:45:13 -0400

    update

--- 07.По-сложни цикли/15.Dumb_passwords.py
+++ 07.По-сложни цикли/15.Dumb_passwords.py
@@ -9,4 +9,4 @@ for num_1 in range(1,n):
             for num_4 in range(97, last_letter):
                 for num_5 in range(1, n+1):
                     if num_5 > num_1 and num_5 > num_2:
-                        print(str(num_1) + str(num_1) + chr(num_3) + chr(num_4) + str(num_5), "", end="")
+                        print(str(num_1) + str(num_2) + chr(num_3) + chr(num_4) + str(num_5), "", end="")
