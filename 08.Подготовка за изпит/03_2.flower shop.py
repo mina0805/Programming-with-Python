407e2daf2bfe3c6c0b9af7db99b52af7f7b748e5
mina0805 <mina.burke@hotmail.com>
2019-07-04 21:45:43 -0400

    update3

--- 08.Подготовка за изпит/03_2.flower shop.py
+++ 08.Подготовка за изпит/03_2.flower shop.py
@@ -10,21 +10,24 @@ if season == "summer" or season == "spring":
     roses_p = 4.1
     tulip_p = 2.5
     total_p = chr * chr_p + roses * roses_p + tulips * tulip_p
-    print(total_p)
+    if holiday == "y":
+        total_p *=1.15
+        if tulips >= 7 and season == "spring":
+            total_p *= 0.95
+        if roses + chr + tulips >=20:
+            total_p *= 0.8
 
 elif season == "winter" or season == "autumn":
     chr_p = 3.75
     roses_p = 4.5
     tulip_p = 4.15
     total_p = chr * chr_p + roses * roses_p + tulips * tulip_p
-    print(total_p)
-if holiday == "y":
-    holiday_p = total_p * 1.15
-    if tulips >= 7 and season == "spring":
-        holiday_p *= 0.95
-    if roses >= 10 and season == "winter":
-        holiday_p *= 0.9
-    if tulips + chr + roses >= 20:
-        holiday_p *= 0.8
-        print(holiday_p)
+    if holiday == "y":
+        total_p *= 1.15
+        if roses >=10 and season == "winter":
+            total_p = total_p * 0.9
+        if roses + chr + tulips >=20:
+            total_p *= 0.8
+
+print(total_p+2)
 
