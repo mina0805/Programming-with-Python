fd00c07760c4da52023df5a99e22544cdcdc4c25
mina0805 <mina.burke@hotmail.com>
2019-07-28 22:09:21 -0400

    update3

--- 08.Подготовка за изпит/01.Paint a house.py
+++ 08.Подготовка за изпит/01.Paint a house.py
@@ -2,7 +2,7 @@ x = float(input("enter house height: "))
 y = float(input("enter length of house: "))
 h = float(input("enter height of roof: "))
 
-front_side = (x * x) - (1.2 *2)
+fron_side = (x * x) - (1.2 *2)
 back_side = x * x
 sides = (x * y * 2) - (1.5*1.5*2)
 green = (front_side+back_side+sides)/3.4
