780c17e8227a68d94b07d114d3a9ddb2da7df22b
mina0805 <mina.burke@hotmail.com>
2019-08-02 05:31:32 -0400

    update3

--- 08.Подготовка за изпит/02.Styrofoam.py
+++ 08.Подготовка за изпит/02.Styrofoam.py
@@ -9,7 +9,7 @@ import math
 net_house_area = (house_area - windows*2.4)*1.1
 styrofoam_nbr_pcg = math.ceil(net_house_area/styrofoam_sq_m)
 total_price = styrofoam_pcg_prc*styrofoam_nbr_pcg
-if total_price <= budget:
+if total_price < budget:
     print("Spent: " + str("%.2f"% total_price))
     print("Left: "+ (str("%.2f" %(budget-total_price))))
 else:
