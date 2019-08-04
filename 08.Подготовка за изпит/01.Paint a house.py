43bea0528792a19cc151f2ab359062126736c602
mina0805 <mina.burke@hotmail.com>
2019-08-03 23:24:03 -0400

    update3

--- 08.Подготовка за изпит/01.Paint a house.py
+++ 08.Подготовка за изпит/01.Paint a house.py
@@ -7,7 +7,7 @@ back_side = x * x
 sides = (x * y * 2) - (1.5*1.5*2)
 green = (front_side+back_side+sides)/3.4
 
-roof = x*y*2 - x*h
+roof = x*y*2 + x*h
 red = roof/4.3
 print('%.2f' % green)
 print('%.2f' % red)
