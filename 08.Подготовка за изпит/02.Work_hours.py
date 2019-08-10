1ae83469607aa675b161030a3a04075d535ab180
mina0805 <mina.burke@hotmail.com>
2019-08-09 23:07:04 -0400

    update3

--- 08.Подготовка за изпит/02.Work_hours.py
+++ 08.Подготовка за изпит/02.Work_hours.py
@@ -5,7 +5,7 @@ penalties = (hours_needed - (workers*days*8))*days
 
 # 1 worker works 8 hours a day
 
-if hours_needed <= workers*days*8:
+if hours_needed <= workers*days*9:
     print(str(workers*days*8 - hours_needed) + " hours left")
 else:
     print(str(hours_needed - (workers*days*8)) + " overtime")
