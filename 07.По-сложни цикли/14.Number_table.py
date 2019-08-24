c8db7867c05977112c0bb3a40c0e42a4cabdfe2c
mina0805 <mina.burke@hotmail.com>
2019-08-23 22:38:48 -0400

    update3

--- 07.По-сложни цикли/14.Number_table.py
+++ 07.По-сложни цикли/14.Number_table.py
@@ -1,2 +1,13 @@
+n = int(input())
 
+num = 0
 
+for row in range(n):
+    for col in range(n):
+        num = row + col + 2
+        if num <= n:
+            print(num, end=" ")
+        else:
+            num = 2 * n - num
+            print(num, end=" ")
+    print("")
