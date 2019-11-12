1541b8f84e8b6725a34fcbd8863c93a9a820fcae
mina0805 <mina.burke@hotmail.com>
2019-11-11 22:49:58 -0500

    update3

--- 02.ПРОСТИ ПРЕСМЯТАНИЯ/test.py
+++ 02.ПРОСТИ ПРЕСМЯТАНИЯ/test.py
@@ -1,7 +1,42 @@
-a = float(input('enter a: '))
-b = float(input('enter b: '))
-h = float(input("enter h: "))
+USD = 1.79549
+EUR = 1.95583
+GBP = 2.53405
+BGN = 1
 
-area = ((a + b)*h)/2
-print(str("%.2f" % area) + " square sm")
+amount = float(input())
+cur_from = input()
+cur_to = input()
+input_amount = 1
+output_amount = 1
+
+if cur_from == "USD":
+    input_amount *= 1.79549
+elif cur_from == "EUR":
+    input_amount *= 1.95583
+elif cur_from == "GBP":
+    input_amount *=2.53405
+elif cur_from == "BGN":
+    input_amount *= 1
+
+if cur_to == "USD":
+    output_amount /= 1.79549
+elif cur_to == "EUR":
+    output_amount /= 1.95583
+elif cur_to == "GBP":
+    output_amount /=2.53405
+elif cur_to == "BGN":
+    output_amount /= 1
+final_amount = (amount*input_amount)/output_amount
+print(final_amount)
+
+
+
+#1 bgn = 1/1.79549
+#12.35 eur to gbp
+#print("%.2f" % ((12.35*1.95583)/2.53405))
+
+
+
+
+
 
