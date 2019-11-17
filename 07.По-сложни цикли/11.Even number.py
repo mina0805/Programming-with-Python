bbd743581e633b79b1c359f5cc148cb85519099c
mina0805 <mina.burke@hotmail.com>
2019-11-17 17:10:36 -0500

    update3

---
+++ 07.По-сложни цикли/11.Even number.py
@@ -0,0 +1,8 @@
+n = int(input('Enter even number: '))
+
+while n%2 > 0:
+    print('Invalid number')
+    n = int(input('Enter even number: '))
+if n%2 == 0:
+    print(str("Even number entered: ") + (str(n)))
+
