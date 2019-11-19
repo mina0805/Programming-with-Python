05fc732c6d04d1f91542fbee9871a7a0c6ea1155
mina0805 <mina.burke@hotmail.com>
2019-11-18 21:13:52 -0500

    update3

--- 08.Подготовка за изпит/04.Grades.py
+++ 08.Подготовка за изпит/04.Grades.py
@@ -4,8 +4,10 @@ between_4_and_499 = 0
 between_3_and_399 = 0
 fail = 0
 average = 0
+sum_grade = 0
 for i in range(1, n+1):
     grade = float(input())
+    sum_grade += grade
 #    Ред 1 - "Top students: {процент студенти с успех 5.00 или повече}%"
 #   Ред 2 - "Between 4.00 and 4.99: {между 4.00 и 4.99 включително}%"
 #    Ред 3 - "Between 3.00 and 3.99: {между 3.00 и 3.99 включително}%"
@@ -30,6 +32,6 @@ print(("Top students: " + (str('{0:.2%}'.format(top_student/n)))))
 print(("Between 4.00 and 4.99: " + (str('{0:.2%}'.format(between_4_and_499/n)))))
 print(("Between 3.00 and 3.99: " + (str('{0:.2%}'.format(between_3_and_399/n)))))
 print(("Fail: " + (str('{0:.2%}'.format(fail/n)))))
-print(average)
+print(("Average: " + (str("%.2f" %(sum_grade/n)))))
 
 
