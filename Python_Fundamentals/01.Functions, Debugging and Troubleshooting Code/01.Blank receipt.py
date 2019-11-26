1a0e351ab25fb4fbd047a7ff5d2f9b462c1efc33
mina0805 <mina.burke@hotmail.com>
2019-11-26 17:51:26 -0500

    update_11242019

---
+++ Python_Fundamentals/01.Functions, Debugging and Troubleshooting Code/01.Blank receipt.py
@@ -0,0 +1,10 @@
+def blank_receipt():
+    print("CASH RECEIPT\n------------------------------")
+    print("Charged to_________\nReceived by________\n------------------------------")
+    print("© SoftUni")
+
+
+blank_receipt()
+
+
+
