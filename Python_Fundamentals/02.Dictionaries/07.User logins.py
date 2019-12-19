7fa65fd25a73ee27f02c4730e2ea8788ad018504
mina0805 <mina.burke@hotmail.com>
2019-12-18 19:18:45 -0500

    update_11242019

---
+++ Python_Fundamentals/02.Dictionaries/07.User logins.py
@@ -0,0 +1,25 @@
+raw_data = input()
+
+login_dict = {}
+unsuccessful_logins = 0
+
+while raw_data != 'login':
+    login, password = raw_data.split('->')
+    for i in raw_data:
+        if i != " ":
+            login_dict[login] = password
+    raw_data = input()
+
+raw_data = input()
+
+while raw_data != 'end':
+    login, password = raw_data.split(' -> ')
+    if login_dict.get(login) == password:
+        print(f'{login}: logged in successfully')
+    else:
+        print(f'{login}: login failed')
+        unsuccessful_logins += 1
+
+    raw_data = input()
+
+print("unsuccessful login attempts:", unsuccessful_logins)
