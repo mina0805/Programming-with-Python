f9f4ad6b35ba3399b46a6c2b45754c909101f277
mina0805 <mina.burke@hotmail.com>
2019-12-21 23:43:06 -0500

    update_11242019

--- Python_Fundamentals/02.Dictionaries/07.User logins.py
+++ Python_Fundamentals/02.Dictionaries/07.User logins.py
@@ -4,18 +4,22 @@ login_dict = {}
 unsuccessful_logins = 0
 
 while raw_data != 'login':
-    login, password = raw_data.split('->')
-    for i in login, password:
-        if i != " ":
-            login_dict[login] = password
+    user_name, password = raw_data.split('->')
+    #for i in login, password:
+        #if i != " ":
+    login_dict[user_name] = password
+    raw_data = input()
+
+raw_data = input()
 
 while raw_data != "end":
-    login, password = raw_data.split('->')
-    for i in login, password:
-        if i != " ":
-            if login_dict[login] == password:
-                print(f'{login}: logged in successfully')
-            else:
-                print(f'“{login}: login failed”. ')
-                unsuccessful_logins +=1
-                print(f'unsuccessful login attempts: {unsuccessful_logins}')
+    user_name, password = raw_data.split('->')
+    if login_dict[user_name] == password:
+        print(f'{user_name}: logged in successfully')
+        raw_data = input()
+    else:
+        print(f'“{user_name}: login failed”. ')
+        unsuccessful_logins += 1
+        raw_data = input()
+
+print(f'unsuccessful login attempts: {unsuccessful_logins}')
