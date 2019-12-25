7cdf6c2139e9a292680990d696974f2bce227dbb
mina0805 <mina.burke@hotmail.com>
2019-12-25 16:00:56 -0500

    update_11242019

--- Python_Fundamentals/02.Dictionaries/08.Filter base.py
+++ Python_Fundamentals/02.Dictionaries/08.Filter base.py
@@ -2,8 +2,23 @@ data = input()
 dict_age = {}
 dict_salary = {}
 dict_position = {}
-
+master_dict = {}
 while data != "filter base":
+    key, value = data.split("->")
+    master_dict[key] = value
+    data = input()
+    print(master_dict)
+    try:
+        master_dict[value]
+
+    except ValueError:
+        print('bla')
+
+
+
+
+
+
 
 
 
