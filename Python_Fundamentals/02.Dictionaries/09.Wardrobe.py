4df45a429f86a8e512098e18506ec1a0ebaa6ffb
mina0805 <mina.burke@hotmail.com>
2019-12-30 06:44:20 -0500

    update_11242019

--- Python_Fundamentals/02.Dictionaries/09.Wardrobe.py
+++ Python_Fundamentals/02.Dictionaries/09.Wardrobe.py
@@ -1,28 +1,27 @@
 n = int(input())
 
-clothes_list = []
-dict_clothes = {}
+clothest_list = []
+clothes_dict = {}
 
-for i in range(0, n):
-    raw_input = input()
-    data = raw_input.split(' -> ')
+for i in range(n):
+    data = input().split(" -> ")
     color = data[0]
-    clothes_list = data[1].split(',')
+    clothest_list = data[1].split(",")
 
-    if color not in dict_clothes:
-        dict_clothes[color] = {}
+    if color not in clothes_dict:
+        clothes_dict[color] = {}
 
-    for item in clothes_list:
-        if item in dict_clothes[color]:
-            dict_clothes[color][item] = dict_clothes[color].get(item) + 1
+    for item in clothest_list:
+        if item in clothes_dict:
+            clothes_dict[color][item] = clothes_dict[color].get(item) + 1
         else:
-            dict_clothes[color][item] = 1
+            clothes_dict[color][item] = 1
+searched_item = input().split()
 
-lookup_item = input().split()
-for key in dict_clothes:
-    print(f'{key} clothes:')
-    for val in dict_clothes[key]:
-        if lookup_item[0] == key and lookup_item[1] == val:
-            print(f'* {val} - {dict_clothes[key][val]} (found!)')
-        else:
-            print(f'* {val} - {dict_clothes[key][val]}')
+    for key in clothes_dict:
+        print(f'{key} clothes:')
+        for value in clothes_dict:
+            if searched_item[0] == key and searched_item[1] == value:
+                print(f'*{value} -{clothes_dict[key][value]} found!')
+            else:
+                print(f'*{value} - {clothes_dict[key][value]}')
