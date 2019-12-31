c1f21d23073e3d44ccd2c3f89aea80776b5d3d09
mina0805 <mina.burke@hotmail.com>
2019-12-30 19:33:14 -0500

    update_11242019

--- Python_Fundamentals/02.Dictionaries/09.Wardrobe.py
+++ Python_Fundamentals/02.Dictionaries/09.Wardrobe.py
@@ -1,18 +1,19 @@
 n = int(input())
 
-clothest_list = []
+clothes_list = []
 clothes_dict = {}
 
 for i in range(n):
-    data = input().split(" -> ")
+    user_entry = input()
+    data = user_entry.split(" -> ")
     color = data[0]
-    clothest_list = data[1].split(",")
+    clothes_list = data[1].split(",")
 
     if color not in clothes_dict:
         clothes_dict[color] = {}
 
-    for item in clothest_list:
-        if item in clothes_dict:
+    for item in clothes_list:
+        if item in clothes_dict[color]:
             clothes_dict[color][item] = clothes_dict[color].get(item) + 1
         else:
             clothes_dict[color][item] = 1
@@ -20,10 +21,10 @@ searched_item = input().split()
 
 for key in clothes_dict:
     print(f'{key} clothes:')
-    for value in clothes_dict:
+    for value in clothes_dict[key]:
         if searched_item[0] == key and searched_item[1] == value:
-            print(f'*{value} -{clothes_dict[key][value]} (found!)')
+            print(f'* {value} -{clothes_dict[key][value]} (found!)')
         else:
-            print(f'*{value} -{clothes_dict[value]}')
-#
-            #print(f'* {value} - {clothes_dict[key][value]}')
+            print(f'* {value} - {clothes_dict[key][value]}')
+
+
