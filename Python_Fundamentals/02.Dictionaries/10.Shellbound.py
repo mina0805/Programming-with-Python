179458cfdcb1237ebe290f8c87ea169507783321
mina0805 <mina.burke@hotmail.com>
2020-01-01 20:58:59 -0500

    update_11242019

--- Python_Fundamentals/02.Dictionaries/10.Shellbound.py
+++ Python_Fundamentals/02.Dictionaries/10.Shellbound.py
@@ -1,15 +1,19 @@
 raw_data = input().lower()
 shells = {}
-size = []
+
 
 while raw_data != "aggregate":
-    key, value = raw_data.split()
-    shells = raw_data[0]
-    size = raw_data[1]
+    lst = list(map(str.strip, raw_data.split(" ")))
+    dict_key = lst[0]
+    dict_value = lst[1]
+    if dict_key not in shells:
+        shells[dict_key] = {}
+    if dict_value.isnumeric():
+        shells[dict_key] = int(dict_value)
     raw_data = input()
 
-    print(shells)
-    print(size)
+
+print(shells)
 
 
 
