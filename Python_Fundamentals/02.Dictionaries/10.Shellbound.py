adb2d334dff860d4eb9e5e4db5e68c20eaee2ecc
mina0805 <mina.burke@hotmail.com>
2020-01-02 19:14:09 -0500

    update_11242019

--- Python_Fundamentals/02.Dictionaries/10.Shellbound.py
+++ Python_Fundamentals/02.Dictionaries/10.Shellbound.py
@@ -1,36 +1,19 @@
-raw_data = input().lower()
+raw_data = input()
 shells = {}
 
 
-while raw_data != "aggregate":
-    lst = list(map(str.strip, raw_data.split(" ")))
-    dict_key = lst[0]
-    dict_value = lst[1]
-    if dict_key not in shells:
-        shells[dict_key] = int(dict_value)
-    else:
-        if __name__ == '__main__':
-            shells[dict_key] =
-    raw_data = input().lower()
+while raw_data != "Aggregate":
+    region, size = raw_data.split()
+    if region not in shells:
+        shells[region] = []
+    if int(size) not in shells[region]:
+        shells[region].append(int(size))
 
-for item in shells:
-    print(item)
+    raw_data = input()
+for key in shells:
+    giant_shell = (sum(shells[key]) - int((sum(shells[key])//len(shells[key]))))
 
-print(shells)
-
-#If I can rephrase your question,
-# what you want is a dictionary with the years as keys
-# and an array for each year containing a list of values associated with that year, right? Here's how I'd do it:
-
-years_dict = dict()
-
-for line in list:
-    if line[0] in years_dict:
-        # append the new number to the existing array at this slot
-        years_dict[line[0]].append(line[1])
-    else:
-        # create a new array in this slot
-        years_dict[line[0]] = [line[1]]
-
-
+    print(f'{key} -> ', end='')
+    print(f'{str(shells[key])}', end='')
+    print(f' ({giant_shell})')
 
