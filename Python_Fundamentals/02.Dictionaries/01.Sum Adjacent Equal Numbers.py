0b7694b3f00b378db929cce48f730dfd4d8b041d
mina0805 <mina.burke@hotmail.com>
2020-01-06 18:55:26 -0500

    update_11242019

--- Python_Fundamentals/02.Dictionaries/01.Sum Adjacent Equal Numbers.py
+++ Python_Fundamentals/02.Dictionaries/01.Sum Adjacent Equal Numbers.py
@@ -1,9 +1,18 @@
 list_num = list(map(int, input().split(" ")))
 print(list_num)
-for i in list_num:
-    if list_num[0] == list_num[1]:
+#for i in list_num:
+    #if list_num[0] == list_num[1]:
 
-        list_num[0] = list_num[0]+list_num[1]
-        list_num.remove(list_num[1])
+        #list_num[1] = list_num[0]+list_num[1]
+        #list_num.remove(list_num[0])
+
+i = 0
+while i < (len(list_num)-1):
+    if list_num[i] == list_num[i+1] and 1 >= 0:
+        list_num[i] += list_num[i+1]
+        list_num.remove(list_num[i+1])
+        i -= 1
+    else:
+        i += 1
 print(list_num)
 
