d319585f93bfe57f80da54957fd5bc8eb1590be5
mina0805 <mina.burke@hotmail.com>
2020-01-10 18:09:00 -0500

    update_11242019

--- Python_Fundamentals/02a. Lists exercises_lab/02. Split by Word Casing.py
+++ Python_Fundamentals/02a. Lists exercises_lab/02. Split by Word Casing.py
@@ -11,7 +11,7 @@ for i in seperator_list:
 new_list = raw_data.split()
 
 for i in new_list:
-    if i == i.lower() or i.isalpha():
+    if i == i.lower() and i.isalpha():
         lower_list.append(i)
     elif i == i.upper() and i.isalpha():
         upper_list.append(i)
