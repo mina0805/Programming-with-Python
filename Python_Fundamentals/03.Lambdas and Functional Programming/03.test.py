405308a44956ab6d5ab8e35145a92cfaf01a0afe
mina0805 <mina.burke@hotmail.com>
2020-01-17 22:41:19 -0500

    update_11242019

--- Python_Fundamentals/03.Lambdas and Functional Programming/03.test.py
+++ Python_Fundamentals/03.Lambdas and Functional Programming/03.test.py
@@ -15,8 +15,12 @@ print(sort_by_value)
 
 # 3 sort by value and then by key
 sort_by_value_and_key = sorted(student_grades.items(), key = lambda kvp: (kvp[1], kvp[0]))
-print(sort_by_value_and_key)
+print(*sort_by_value_and_key)
 
-# sorted by the length of the values and then by key
+# 4 sorted by the length of the values and then by key
 sorted_by_length = sorted(student_grades.items(), key = lambda kvp: (len(kvp[1]), kvp[0]))
 print(sorted_by_length)
+
+# 5 sort both key and value by descending order o
+sorted_by_desc_ordr = sorted(student_grades.items(), key = lambda kvp: (len(kvp[1]), kvp[0]), reverse = True)
+print(sorted_by_desc_ordr)
