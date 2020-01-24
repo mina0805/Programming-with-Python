6c53414293afd320940bd3b57b2d017b2cb838e0
mina0805 <mina.burke@hotmail.com>
2020-01-24 18:14:48 -0500

    update_11242019

--- Python_Fundamentals/02a. Lists exercises_lab/04.Objects and Classes/02.Closest Two Points.py
+++ Python_Fundamentals/02a. Lists exercises_lab/04.Objects and Classes/02.Closest Two Points.py
@@ -1,9 +1,12 @@
 n = int(input())
-raw_data = input()
+raw_data = float(input())
 point_list = []
 for i in range(n):
     point_list.append(raw_data)
-    raw_data = input()
+    raw_data = float(input())
 
-print(point_list)
-    
+
+# sort all x-points in a list from smallest to largest
+# sort all y-points in another list from smallest to largest
+# find the pair of x-points with the shortest distance between them
+
